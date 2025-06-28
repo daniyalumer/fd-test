97ec43a174cea8f93700e229174a1cbbbc7fc092
daniyalumer <daniyalharal100@gmail.com>
2025-06-28 14:19:49 +0500

    Pydantic Models For Response

---
+++ models/weather.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+
+class Location(BaseModel):
+    city: str
+    country: str
+
+class CurrentWeather(BaseModel):
+    temperature: float
+    condition: str
+    humidity: float
+    wind_speed: float
+    feels_like: float
+
+class HourlyForecast(BaseModel):
+    time: str
+    temperature_2m: float
+    condition: str
+    precipitation_probability: int
+    feels_like: float
+
+class CurrentWeatherResponse(BaseModel):
+    location: Location
+    current_weather: CurrentWeather
+
+class WeatherForecastResponse(BaseModel):
+    location: Location
+    hourly_forecast: list[HourlyForecast]
