e83d060a027a2341229eae5760db53274518ab4a
daniyalumer <daniyalharal100@gmail.com>
2025-06-28 17:55:40 +0500

    Registering Weather Analysis Routes

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException
-from controllers import weather
+from fastapi import APIRouter
+from controllers import weather, weather_analysis
 
 router = APIRouter()
 
@@ -7,3 +7,6 @@ router.include_router(
     weather.router,
     prefix="/api")
 
+router.include_router(
+    weather_analysis.router,
+    prefix="/api")
